ecd62d110c26bd666ef24f87beb5117de798d3d4
Ciara R. <100184461+creed725@users.noreply.github.com>
2025-03-01 12:41:38 -0600

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateTravel(event) {
-  event.preventDefault();
-
+function displayTravel(response) {
+  console.log("travel generated");
   new Typewriter("#travel", {
-    strings: ["The travel will go here"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateTravel(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "40bdb8c3a26579atfoa8a2d376def906";
+  let context =
+    "You are a travel expert and love to plan trips. Your mission is to generate travel plans in basic HTML and separate each line with a <br />. The title should begin with `Travel Plans for`. Sign the travel guide with `Your AI Travel Guide` inside a <strong> element at the end of the travel plans. Do not include the ```htm`. Make sure to follow the user instructions. ";
+  let prompt = `User instructions: Generate travel information for popular tourist attractions, popular eateries, local currency and best means of travel while visting ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating travel");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayTravel);
 }
 
 let travelFormElement = document.querySelector("#travel-generator-form");

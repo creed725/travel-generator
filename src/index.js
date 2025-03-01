eabb0f7b8d6328f3ff56bc994fb6b65c07fbd1f7
Ciara R. <100184461+creed725@users.noreply.github.com>
2025-03-01 14:37:55 -0600

    Improved the User Experience (UX)

--- src/index.js
+++ src/index.js
@@ -14,9 +14,13 @@ function generateTravel(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "40bdb8c3a26579atfoa8a2d376def906";
   let context =
-    "You are a travel expert and love to plan trips. Your mission is to generate travel plans in basic HTML and separate each line with a <br />. The title should begin with `Travel Plans for`. Sign the travel guide with `Your AI Travel Guide` inside a <strong> element at the end of the travel plans. Do not include the ```htm`. Make sure to follow the user instructions. ";
+    "You are a travel expert and love to plan trips. Your mission is to generate travel plans in basic HTML and separate each line with a <br />. The title should begin with `Travel Guide for`. Use a header for each travel information section and ol for the information. Sign the travel guide with `Your AI Travel Guide` inside a <strong> element at the end of the travel plans. Do not include the ```htm`. Make sure to follow the user instructions. ";
   let prompt = `User instructions: Generate travel information for popular tourist attractions, popular eateries, local currency and best means of travel while visting ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let destinationElement = document.querySelector("#travel");
+  destinationElement.classList.remove("hidden");
+  destinationElement.innerHTML = `<div class="generating">⏳Generating the travel guide for ${instructionsInput.value}</div>`;
 
   console.log("Generating travel");
   console.log(`Prompt: ${prompt}`);
